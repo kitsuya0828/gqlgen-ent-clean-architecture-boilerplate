67c724c8bf826df265db8ed27dad1f00e41267dd
Kitsuya0828 <kitsuyaazuma@gmail.com>
2023-09-03 15:21:52 +0900

    feat: add app config

---
+++ config/config.go
@@ -0,0 +1,80 @@
+package config
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"path"
+	"path/filepath"
+	"runtime"
+
+	"github.com/spf13/viper"
+	"github.com/Kitsuya0828/gqlgen-ent-clean-architecture-boilerplate/pkg/util/environment"
+)
+
+type config struct {
+	Database struct {
+		User                 string
+		Password             string
+		Net                  string
+		Addr                 string
+		DBName               string
+		AllowNativePasswords bool
+		Params               struct {
+			ParseTime string
+			Charset   string
+			Loc       string
+		}
+	}
+	Server struct {
+		Address string
+	}
+}
+
+// C is config variable
+var C config
+
+// ReadConfigOption is a config option
+type ReadConfigOption struct {
+	AppEnv string
+}
+
+// ReadConfig configures config file
+func ReadConfig(option ReadConfigOption) {
+	Config := &C
+
+	if environment.IsDev() {
+		viper.AddConfigPath(filepath.Join(rootDir(), "config"))
+		viper.SetConfigName("config")
+	} else if environment.IsTest() || (option.AppEnv == environment.Test) {
+		fmt.Println(rootDir())
+		viper.AddConfigPath(filepath.Join(rootDir(), "config"))
+		viper.SetConfigName("config.test")
+	} else if environment.IsE2E() || (option.AppEnv == environment.E2E) {
+		fmt.Println(rootDir())
+		viper.AddConfigPath(filepath.Join(rootDir(), "config"))
+		viper.SetConfigName("config.e2e")
+	} else {
+		// production configuration here
+		fmt.Println("production configuration here")
+	}
+
+	viper.SetConfigType("yml")
+	viper.AutomaticEnv()
+
+	if err := viper.ReadInConfig(); err != nil {
+		fmt.Println(err)
+		log.Fatalln(err)
+	}
+
+	if err := viper.Unmarshal(&Config); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
+
+func rootDir() string {
+	_, b, _, _ := runtime.Caller(0)
+	d := path.Join(path.Dir(b))
+	return filepath.Dir(d)
+}
